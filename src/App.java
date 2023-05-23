1b08ad0356491143518c9266b89f0a55d293a1dd
Niffrai <71142091+Niffrai@users.noreply.github.com>
2023-05-23 19:22:27 +0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -9,21 +9,17 @@ public class App implements Runnable {
     private static final String PKCS11_LIBRARY_PATH = "/path/to/pkcs11-library.so"; // Path to the PKCS#11 library for eToken
 
     public static void main(String[] args) {
-        // Create and start the monitoring thread
         Thread monitorThread = new Thread(new App());
         monitorThread.start();
 
         try {
-            // Initialize the PKCS#11 library
             Module pkcs11Module = Module.getInstance(PKCS11_LIBRARY_PATH);
             pkcs11Module.initialize(null);
 
-            // Get library information
             System.out.println("Library information:");
             System.out.println("  Library: " + pkcs11Module.getLibraryDescription());
             System.out.println("  Version: " + pkcs11Module.getLibraryVersion());
 
-            // Wait for the monitoring thread to finish
             monitorThread.join();
         } catch (PKCS11Exception | InterruptedException e) {
             e.printStackTrace();
@@ -33,14 +29,10 @@ public class App implements Runnable {
     @Override
     public void run() {
         try {
-            // Create the Provider object using the PKCS#11 library
             PKCS11Provider provider = new PKCS11Provider(PKCS11_LIBRARY_PATH);
             Security.addProvider(provider);
-
-            // Create the TokenManager object for event tracking
             TokenManager tokenManager = new TokenManager();
 
-            // Track token insertion/removal events
             while (true) {
                 TokenEvent event = tokenManager.getTokenEvent();
                 Token token = event.getToken();
