3dabb124e1f8f227783b46986305af7645273eb3
Niffrai <71142091+Niffrai@users.noreply.github.com>
2023-05-23 19:21:59 +0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,61 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.security.Security;
+import iaik.pkcs.pkcs11.*;
+import iaik.pkcs.pkcs11.Module;
+import iaik.pkcs.pkcs11.objects.*;
+import iaik.pkcs.pkcs11.wrapper.PKCS11Exception;
+
+public class App implements Runnable {
+
+    private static final String PKCS11_LIBRARY_PATH = "/path/to/pkcs11-library.so"; // Path to the PKCS#11 library for eToken
+
+    public static void main(String[] args) {
+        // Create and start the monitoring thread
+        Thread monitorThread = new Thread(new App());
+        monitorThread.start();
+
+        try {
+            // Initialize the PKCS#11 library
+            Module pkcs11Module = Module.getInstance(PKCS11_LIBRARY_PATH);
+            pkcs11Module.initialize(null);
+
+            // Get library information
+            System.out.println("Library information:");
+            System.out.println("  Library: " + pkcs11Module.getLibraryDescription());
+            System.out.println("  Version: " + pkcs11Module.getLibraryVersion());
+
+            // Wait for the monitoring thread to finish
+            monitorThread.join();
+        } catch (PKCS11Exception | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            // Create the Provider object using the PKCS#11 library
+            PKCS11Provider provider = new PKCS11Provider(PKCS11_LIBRARY_PATH);
+            Security.addProvider(provider);
+
+            // Create the TokenManager object for event tracking
+            TokenManager tokenManager = new TokenManager();
+
+            // Track token insertion/removal events
+            while (true) {
+                TokenEvent event = tokenManager.getTokenEvent();
+                Token token = event.getToken();
+
+                if (event.getType() == TokenEvent.INSERTION) {
+                    System.out.println("eToken connected");
+                    System.out.println("  Label: " + token.getTokenInfo().getLabel());
+                    System.out.println("  Manufacturer: " + token.getTokenInfo().getManufacturerID());
+                    System.out.println("  Serial Number: " + token.getTokenInfo().getSerialNumber());
+                } else if (event.getType() == TokenEvent.REMOVAL) {
+                    System.out.println("eToken disconnected");
+                }
+            }
+        } catch (PKCS11Exception e) {
+            e.printStackTrace();
+        }
     }
 }
